212f221404436ef3460f8a1953370c4fa6a7618f
CJ Eckley <cjeckley01@gmail.com>
2024-01-29 14:23:24 +0200

    Back change

--- server.js
+++ server.js
@@ -14,13 +14,6 @@ mongoose.connect(
   {}
 );
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://13.244.73.99:3000");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
 const User = mongoose.model("user", {
   email: String,
   password: String,
